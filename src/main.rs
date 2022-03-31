5770b1f7eac539af933eb0954e8dd47a39cddb0e
CrispiCas <ole.siemens@gmx.net>
2022-03-31 21:25:07 +0200

    things idk what :)

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=10);
 
     loop {
-        println!("please input your guess or quit with 'quit':");
+        println!("please input your guess:");
 
 
         //println!("The secret number is: {}", secret_number);
